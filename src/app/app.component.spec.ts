0c71bc5d8c064dcddaa31ca37adfdc46c53307e4
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2023-08-08 14:49:14 -0300

    add ESLint to project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,13 +1,13 @@
-import { TestBed } from "@angular/core/testing";
-import { RouterTestingModule } from "@angular/router/testing";
 import { AppComponent } from "./app.component";
+import { RouterTestingModule } from "@angular/router/testing";
+import { TestBed } from "@angular/core/testing";
 
 describe("AppComponent", () => {
   beforeEach(() =>
     TestBed.configureTestingModule({
       imports: [RouterTestingModule],
       declarations: [AppComponent],
-    })
+    }),
   );
 
   it("should create the app", () => {
