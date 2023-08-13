d5d6ae439ee9ea762814a43be7855c04c3b7616b
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2023-08-13 00:35:50 -0300

    create game module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,34 +6,23 @@ import { BrowserModule } from "@angular/platform-browser";
 import { EffectsModule } from "@ngrx/effects";
 import { NgModule, isDevMode } from "@angular/core";
 import { HttpClientModule } from "@angular/common/http";
-import { MainGameComponent } from "./features/game/components/main-game/main-game.component";
 import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 import { StoreModule } from "@ngrx/store";
 import { metaReducers, reducers } from "../app/state";
-import { CellComponent } from "./features/game/containers/cell/cell.component";
 import { StorageService } from "./services/storage.service";
 import { SettingsComponent } from "./features/settings/components/settings.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { HeaderComponent } from "./features/shared/header/header.component";
 import { TimerService } from "./services/timer.service";
-import { SevenSegmentDisplayComponent } from "./features/game/containers/seven-segment/seven-segment-display.component";
-import { ResetButtonComponent } from "./features/game/containers/reset-button/reset-button.component";
 import { SaveAndLoadComponent } from "./features/save-and-load/components/save-and-load/save-and-load.component";
-import { BoardComponent } from "./features/game/containers/board/board.component";
+import { GameModule } from "./features/game/game/game.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    MainGameComponent,
-    CellComponent,
     SettingsComponent,
-    MainGameComponent,
-    CellComponent,
-    SevenSegmentDisplayComponent,
-    ResetButtonComponent,
     SaveAndLoadComponent,
-    BoardComponent,
   ],
   imports: [
     BrowserModule,
@@ -42,6 +31,7 @@ import { BoardComponent } from "./features/game/containers/board/board.component
     EffectsModule.forRoot([AppEffects]),
     HttpClientModule,
     ReactiveFormsModule,
+    GameModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: !isDevMode() }),
   ],
---
+++ src/app/features/game/game/game.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { CellComponent } from "../containers/cell/cell.component";
+import { ResetButtonComponent } from "../containers/reset-button/reset-button.component";
+import { SevenSegmentDisplayComponent } from "../containers/seven-segment/seven-segment-display.component";
+import { MainGameComponent } from "../components/main-game/main-game.component";
+import { BoardComponent } from "../containers/board/board.component";
+
+@NgModule({
+  declarations: [
+    CellComponent,
+    SevenSegmentDisplayComponent,
+    ResetButtonComponent,
+    CellComponent,
+    MainGameComponent,
+    BoardComponent,
+  ],
+  imports: [CommonModule],
+  exports: [
+    CellComponent,
+    SevenSegmentDisplayComponent,
+    ResetButtonComponent,
+    CellComponent,
+    MainGameComponent,
+    BoardComponent,
+  ],
+})
+export class GameModule {}
