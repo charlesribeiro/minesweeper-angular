9e2f2ead60d5660231fd577283e52b16123007da
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2023-08-13 19:04:55 -0300

    move refactor shared folder file structure into containers and components

--- src/app/features/shared/shared.module.ts
+++ src/app/features/shared/shared.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { ErrorComponent } from "./error/error.component";
-import { LoaderComponent } from "./loader/loader.component";
+import { ErrorComponent } from "./containers/error/error.component";
+import { LoaderComponent } from "./containers/loader/loader.component";
+import { HeaderComponent } from "./containers/header/header.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
-  declarations: [ErrorComponent, LoaderComponent],
-  imports: [CommonModule],
-  exports: [ErrorComponent, LoaderComponent],
+  declarations: [ErrorComponent, LoaderComponent, HeaderComponent],
+  imports: [CommonModule, RouterModule],
+  exports: [ErrorComponent, LoaderComponent, HeaderComponent],
 })
 export class SharedModule {}
