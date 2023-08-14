9ae8edfd5fbce1d5550cb5603ab2d37cf17b0ac3
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2023-08-14 00:19:56 -0300

    create general page-wrapper implementing content projection

--- src/app/features/save-and-load/components/save-and-load/save-and-load.component.ts
+++ src/app/features/save-and-load/components/save-and-load/save-and-load.component.ts
@@ -11,7 +11,6 @@ import { PlayerBoard } from "src/app/models/playerBoard.model";
 @Component({
   selector: "app-save-and-load",
   templateUrl: "./save-and-load.component.html",
-  styleUrls: ["./save-and-load.component.sass"],
 })
 export class SaveAndLoadComponent implements OnInit {
   playerBoard: PlayerBoard;
