e3ed3be79d09a08c1fb085839b6ad7bf2ac73f66
Charles Reis Ribeiro <charles.r.ribeiro@gmail.com>
2023-08-14 13:06:00 -0300

    remove unused style sheets, add OnPush change detection

--- src/app/features/shared/containers/error/error.component.ts
+++ src/app/features/shared/containers/error/error.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "app-error",
   templateUrl: "./error.component.html",
-  styleUrls: ["./error.component.sass"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ErrorComponent {}
--- src/app/features/shared/containers/loader/loader.component.ts
+++ src/app/features/shared/containers/loader/loader.component.ts
@@ -1,8 +1,8 @@
-import { Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from "@angular/core";
 
 @Component({
   selector: "app-loader",
   templateUrl: "./loader.component.html",
-  styleUrls: ["./loader.component.sass"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class LoaderComponent {}
